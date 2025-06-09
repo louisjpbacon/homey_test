1d16978347d5887922718d1dee7e4ec2c626bae2
Louis Bacon <lbacon@contractor.indeed.com>
2025-06-09 18:42:46 +0100

    Comment errors

--- spec/system/project_conversation_spec.rb
+++ spec/system/project_conversation_spec.rb
@@ -5,22 +5,39 @@ RSpec.describe "Project conversation history", type: :system do
     driven_by(:rack_test)
   end
 
-  it "allows a user to comment and change status" do
-    user = create(:user)
-    project = create(:project)
+  context "with a valid comment" do
+    it "allows a user to comment and change status" do
+      user = create(:user)
+      project = create(:project)
 
-    visit project_path(project)
+      visit project_path(project)
 
-    fill_in "Comment", with: "This is my first comment!"
-    click_button "Post Comment"
+      fill_in "Comment", with: "This is my first comment!"
+      click_button "Post Comment"
 
-    expect(page).to have_content("This is my first comment!")
-    expect(page).to have_content(user.name)
+      expect(page).to have_content("This is my first comment!")
+      expect(page).to have_content(user.name)
 
-    select "In progress", from: "Status"
-    click_button "Update Status"
+      select "In progress", from: "Status"
+      click_button "Update Status"
 
-    expect(page).to have_text("Test User changed the status to In progress")
-    expect(page).to have_content(user.name)
+      expect(page).to have_text("Test User changed the status to In progress")
+      expect(page).to have_content(user.name)
+    end
+  end
+
+  context "with an invalid comment" do
+    it "does not allow a user to post an empty comment" do
+      user = create(:user)
+      project = create(:project)
+
+      visit project_path(project)
+
+      fill_in "Comment", with: ""
+      click_button "Post Comment"
+
+      expect(page).to have_content("Could not post comment")
+      expect(page).not_to have_content(user.name)
+    end
   end
 end
